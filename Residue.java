01e9f0cb202bafd775fb1584094f16a7d91bc2c4
Bryn <Bryn@Server.com>
2012-06-27 10:17:20 -0400

    Add Residue.java 6/27 10:17am

---
+++ Residue.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Residue {
+    
+    Integer bFactor;
+    // other state
+
+    //make as many constructors as needed
+    public Residue() {
+    }
+
+    //accessor methods
+
+    //mutator methods
+
+}
