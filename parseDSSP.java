975253eaca87dd5f20320123c3a4514ae089d5b5
Bryn <Bryn@Server.com>
2012-06-27 12:26:41 -0400

    Commit parseDSSP 6/27 12:26pm

---
+++ parseDSSP.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class parseDSSP {
+
+    ArrayList<SecondaryStructure> dsspFile;
+
+    public parseDSSP(ArrayList<SecondaryStructure> dsspFile) {
+	this.dsspFile = dsspFile;
+    }
+}
