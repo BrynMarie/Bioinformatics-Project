52d6b8a3358eb2702653da91faebd5253503b51d
BrynMarie <bronwynmr@hotmail.com>
2012-07-02 11:40:12 -0300

    Update mutator methods and constructors 10.40am 7/2

--- Residue.java
+++ Residue.java
@@ -7,28 +7,37 @@ public class Residue {
     CartesianCoord coords;
     String pdbResNum;
     String ssType;
+    boolean missingSS;
     // other state
 
     //make as many constructors as needed
-    public Residue(double bFactor, String pdbResNum, double x, double y, double z) {
+    public Residue(String pdbResNum, double bFactor, String ssType, double x, double y, double z) {
         this.bFactor = bFactor;
         this.coords = new CartesianCoord(x, y, z);
         this.pdbResNum = pdbResNum;
+        this.ssType = ssType;
     }
     
-    public Residue(double bFactor, String pdbResNum, CartesianCoord coords) {
+    public Residue(String pdbResNum, double bFactor, String ssType, CartesianCoord coords) {
         this.bFactor = bFactor;
         this.pdbResNum = pdbResNum;
         this.coords = coords;
+        this.ssType = ssType;
     }
 
     public Residue() {
     }
+    
     //accessor methods
+    
     
 
     //mutator methods
     public void setSSType(String newSsType) {
 	ssType = newSsType;
+    }
+    
+    public void setMissingSS(boolean missingSS) {
+    	this.missingSS = missingSS;
     }
 }
