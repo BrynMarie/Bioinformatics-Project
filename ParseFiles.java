15535c152b1b60db6ac16a4fed248656642f0217
sugargreenbean <minh.j.van@gmail.com>
2012-07-02 02:56:04 -0300

    calculate z-scores of residues

--- ParseFiles.java
+++ ParseFiles.java
@@ -12,35 +12,93 @@ public class ParseFiles {
         this.dsspFile = dsspFile;
     }
     
-    public ArrayList<String> parsePDB(ArrayList<String> rawPdbFile){
-    	//takes raw PDB file and parses 
-    	//final output parsed pdb
-    	ArrayList<String> parsedPDBFile = new ArrayList<String>();
-    	int countTotalAtoms=0;
-    	double totalbFactor = 0;
-    	double totalsquaredbFactor = 0;
-    	for(int i=0; i<rawPdbFile.size(); i++) {
-    		String[] strs = rawPdbFile.get(i).split("\\s+");
-        	if(strs[0].equals("ATOM")){ //only want lines from PDB that are from ATOM section
-        		String line = strs[1] + " " + strs[2] + " " + strs[3] + " " + strs[6] + " " + strs[7] + " " + strs[8] + " " + strs[10];
-        		parsedPDBFile.add(line);
-        		//calculate mean b-factor and std deviation b-factors for whole protein
-        		countTotalAtoms++;
-        		double strbFactortoDbl = Double.valueOf(strs[10].trim()).doubleValue();
-        		double squaredbFactor = Math.pow(strbFactortoDbl, 2);
-        		totalbFactor += strbFactortoDbl;
-        		totalsquaredbFactor += squaredbFactor;
-        	}
-        	else if(strs[0].equals("TER")){
-        		String line = strs[0] + " " + strs[1] + " " + strs[2] + " " + strs[4];
-        		parsedPDBFile.add(line);
-        	}
-    	}
-    	double meanbFactor = totalbFactor/countTotalAtoms;
-    	parsedPDBFile.add(Double.toString(meanbFactor));
-    	parsedPDBFile.add(Double.toString(calcStdDev(totalbFactor, totalsquaredbFactor, countTotalAtoms)));
-    	return parsedPDBFile;
-    }
+	public double getTotalMean(ArrayList<String> rawPdbFile) {
+		// calculate mean b-factor and std deviation b-factors for whole
+		// protein
+		int countTotalAtoms = 0;
+		double totalbFactor = 0;
+		for (int i = 0; i < rawPdbFile.size(); i++) {
+			String[] strs = rawPdbFile.get(i).split("\\s+");
+			if (strs[0].equals("ATOM")) {
+				countTotalAtoms++;
+				double strbFactortoDbl = Double.valueOf(strs[10].trim())
+						.doubleValue();
+				totalbFactor += strbFactortoDbl;
+			}
+		}
+		double meanbFactor = totalbFactor / countTotalAtoms;
+		return meanbFactor;
+	}
+
+	public double getTotalStdDev(ArrayList<String> rawPdbFile) {
+		int countTotalAtoms = 0;
+		double totalbFactor = 0;
+		double totalsquaredbFactor = 0;
+		for (int i = 0; i < rawPdbFile.size(); i++) {
+			String[] strs = rawPdbFile.get(i).split("\\s+");
+			if (strs[0].equals("ATOM")) {
+				countTotalAtoms++;
+				double strbFactortoDbl = Double.valueOf(strs[10].trim())
+						.doubleValue();
+				double squaredbFactor = Math.pow(strbFactortoDbl, 2);
+				totalbFactor += strbFactortoDbl;
+				totalsquaredbFactor += squaredbFactor;
+			}
+		}
+		double StdDev = calcStdDev(totalbFactor, totalsquaredbFactor,
+				countTotalAtoms);
+		return StdDev;
+	}
+
+	public ArrayList<Double> calculateBfactorZScore(ArrayList<String> rawPdbFile,
+			double totalMean, double totalStdDev) {
+		ArrayList<Double> zScoresOfPDBFile = new ArrayList<Double>();
+		int countTotalAtoms = 0;
+		int currentResidue = 0;
+		int pastResidue = 0;
+		double currentResidueBfactor = 0;
+		long countResidues = 0;
+		double meanOfCurrentResidue = 0;
+		for (int i = 0; i < rawPdbFile.size(); i++) {
+			String[] strs = rawPdbFile.get(i).split("\\s+");
+			if (strs[0].equals("ATOM")) { // only want lines from PDB that are
+											// from ATOM section
+				// calculate total B-factor of a residue
+				if (countTotalAtoms == 0) {
+					pastResidue = Integer.parseInt(strs[5]);
+					countResidues++;
+					currentResidueBfactor += Double.valueOf(strs[10].trim())
+							.doubleValue();
+				} else {
+					currentResidue = Integer.parseInt(strs[5]);
+					if (currentResidue == pastResidue) {
+						countResidues++;
+						currentResidueBfactor += Double
+								.valueOf(strs[10].trim()).doubleValue();
+					} else {
+						meanOfCurrentResidue = currentResidueBfactor
+								/ (countResidues); // calculate mean B-factor of
+													// a
+													// residue
+						zScoresOfPDBFile.add(zScore(meanOfCurrentResidue, totalMean, totalStdDev));
+						countResidues = 0;
+						pastResidue = currentResidue;
+						currentResidueBfactor = 0;
+						currentResidueBfactor += Double
+								.valueOf(strs[10].trim()).doubleValue();
+					}
+				}
+			}
+		}
+		return zScoresOfPDBFile;
+	}
+	
+	public double zScore(double currentMean, double totalMean,
+			double totalStdDev) {
+		double zScore = (currentMean - totalMean) / totalStdDev;
+		return zScore;
+	}
+
     
     public double calcStdDev(double total, double squaredTotal, int counter) {
     	double firstTerm = squaredTotal/(counter-1);
