8042d14d5feac881d10f579ed491462aa4226d23
BrynMarie <bronwynmr@hotmail.com>
2012-07-06 19:26:21 -0300

    Commented out calczscore method

--- AtomToResidue.java
+++ AtomToResidue.java
@@ -23,31 +23,30 @@ public class AtomToResidue {
     }
 
 	public ArrayList<Double> calculateBfactorZScore(ArrayList<String> rawPdbFile, double totalMean, double totalStdDev) {
-		ArrayList<Double> zScoresOfPDBFile = new ArrayList<Double>();
-		int countTotalAtoms = 0;
+		ArrayList<Double> zScoresOfPDBFile = new ArrayList<Double>(); // should be stored in Residue
+		int countTotalAtoms = 0; // can be got from atomList.size();
 		int currentResidue = 0;
 		int pastResidue = 0;
 		double currentResidueBfactor = 0;
 		long countResidues = 0;
 		double meanOfCurrentResidue = 0;
 		for (int i = 0; i < rawPdbFile.size(); i++) {
-			String[] strs = rawPdbFile.get(i).split("\\s+");
+			String[] strs = rawPdbFile.get(i).split("\\s+"); // this doesn't work
 			if (strs[0].equals("ATOM")) { 
-				// calculate total B-factor of a residue
-				if (countTotalAtoms == 0) {
-					pastResidue = Integer.parseInt(strs[5]);
-					countResidues++;
-					currentResidueBfactor += Double.valueOf(strs[10].trim()).doubleValue();
+				if (countTotalAtoms == 0) { // if first atom...this should be expressed differently
+					pastResidue = Integer.parseInt(strs[5]); // what information is this getting out?
+					countResidues++; // increment res count after first atom in that residue?
+					currentResidueBfactor += Double.valueOf(strs[10].trim()).doubleValue(); // += necessary?
 				} else {
 					currentResidue = Integer.parseInt(strs[5]);
-					if (currentResidue == pastResidue) {
+					if (currentResidue == pastResidue) { // if on the same residue, add the next atom's bfactor
 						countResidues++;
 						currentResidueBfactor += Double
 								.valueOf(strs[10].trim()).doubleValue();
-					} else {
+					} else { // if not on the same residue
 						meanOfCurrentResidue = currentResidueBfactor
 								/ (countResidues); // calculate mean B-factor of a residue
-						zScoresOfPDBFile.add(zScore(meanOfCurrentResidue, totalMean, totalStdDev));
+						zScoresOfPDBFile.add(zScore(meanOfCurrentResidue, totalMean, totalStdDev)); // add zscore
 						countResidues = 0;
 						pastResidue = currentResidue;
 						currentResidueBfactor = 0;
