646d1685efca7668eb372ec87e87981810a723ec
BrynMarie <bronwynmr@hotmail.com>
2012-07-06 16:57:24 -0300

    This should work like a boss

--- AtomComparator.java
+++ AtomComparator.java
@@ -3,8 +3,7 @@ import java.io.*;
 
 //to be used to sort atoms according to which residue they are in.
 public class AtomComparator implements Comparator<Atom> {
-
-	//getResNum() make sure method exists
+	
     public int compare(Atom a1, Atom a2) {
 		return a1.getResNum().compareTo(a2.getResNum());
     }
