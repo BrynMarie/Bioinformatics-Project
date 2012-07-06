35542ec3f5f32e8af4beddfcda4abe581cf2206f
BrynMarie <bronwynmr@hotmail.com>
2012-07-06 16:56:43 -0300

    Updated - make sure getResNum() exists in Atom.java

--- AtomComparator.java
+++ AtomComparator.java
@@ -4,19 +4,12 @@ import java.io.*;
 //to be used to sort atoms according to which residue they are in.
 public class AtomComparator implements Comparator<Atom> {
 
+	//getResNum() make sure method exists
     public int compare(Atom a1, Atom a2) {
-	if(a1.resNum > a2.resNum) {
-	    return 1;
-	}
-	else if(a1.resNum < a2.resNum) {
-	    return -1;
-	}
-	else {
-	    return 0;
-	}
+		return a1.getResNum().compareTo(a2.getResNum());
     }
 
     public boolean equals(Object obj){
-	return this.equals(obj);
+		return this.equals(obj);
     }
 }
