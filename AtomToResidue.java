7a648b773cc47179ebf3703809756b73342cc246
sugargreenbean <minh.j.van@gmail.com>
2012-07-08 20:26:20 -0300

    commented on calculateBfactorZscore

--- AtomToResidue.java
+++ AtomToResidue.java
@@ -21,36 +21,39 @@ public class AtomToResidue {
     		
     	}
     }
-
+//this method will change once we figure out what we're doing with residues and the arrayList of atoms that they're
+//supposed to contain, but I made some immediate changes to answer the comments
 	public ArrayList<Double> calculateBfactorZScore(ArrayList<String> rawPdbFile, double totalMean, double totalStdDev) {
 		ArrayList<Double> zScoresOfPDBFile = new ArrayList<Double>(); // should be stored in Residue
-		int countTotalAtoms = 0; // can be got from atomList.size();
+		int countTotalResidueAtoms = 0; //I need this integer to count the residue's number of atoms, not the total
+		//number of atoms in the whole protein from atomList.size();
 		int currentResidue = 0;
 		int pastResidue = 0;
 		double currentResidueBfactor = 0;
 		long countResidues = 0;
 		double meanOfCurrentResidue = 0;
-		for (int i = 0; i < rawPdbFile.size(); i++) {
-			String[] strs = rawPdbFile.get(i).split("\\s+"); // this doesn't work
-			if (strs[0].equals("ATOM")) { 
-				if (countTotalAtoms == 0) { // if first atom...this should be expressed differently
-					pastResidue = Integer.parseInt(strs[5]); // what information is this getting out?
-					countResidues++; // increment res count after first atom in that residue?
-					currentResidueBfactor += Double.valueOf(strs[10].trim()).doubleValue(); // += necessary?
+		for (int i = 0; i< rawPdbFile.size(); ++i) {
+		if(rawPdbFile.get(i).substring(0,6).trim().equals("ATOM")) {
+     			String[] strs = customPDBSplit(rawPdbFile.get(i));
+				if (i == 0) { 
+					pastResidue = Integer.parseInt(strs[3]); 
+					countTotalResidueAtoms++;
+					currentResidueBfactor += Double.valueOf(strs[8].trim()).doubleValue(); // += necessary?
+					//i need this to add up the b-factor value across the residue
 				} else {
 					currentResidue = Integer.parseInt(strs[5]);
 					if (currentResidue == pastResidue) { // if on the same residue, add the next atom's bfactor
-						countResidues++;
+						countTotalResidueAtoms++;
 						currentResidueBfactor += Double
 								.valueOf(strs[10].trim()).doubleValue();
 					} else { // if not on the same residue
 						meanOfCurrentResidue = currentResidueBfactor
-								/ (countResidues); // calculate mean B-factor of a residue
+								/ (countTotalResidueAtoms); // calculate mean B-factor of a residue
 						zScoresOfPDBFile.add(zScore(meanOfCurrentResidue, totalMean, totalStdDev)); // add zscore
 						countResidues = 0;
 						pastResidue = currentResidue;
 						currentResidueBfactor = 0;
-						currentResidueBfactor += Double.valueOf(strs[10].trim()).doubleValue();
+						currentResidueBfactor += Double.valueOf(strs[8].trim()).doubleValue();
 					}
 				}
 			}
