ea6f32e83bb2dc46b198cd35a34c4384c112d520
Bryn <Bryn@Server.com>
2012-07-10 18:06:35 -0400

    adding residuetoss.java

---
+++ ResidueToSS.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class ResidueToSS {
+
+    //this class will take an array of residues, some of which are missing in the dssp file or the pdb file. It has not been filtered of loops that are too long, either.
+
+    //TODO: Filter out loops that are greater than 12 residues long
+    //TODO: Discard loops with missing residues and/or main chain atoms
+    //TODO: Filter residues with too high of a zscore
+    //look over coding ideas docs before going too far on this one.
+    public ResidueToSS(ArrayList<Residue> resArray) {
+    }
+
+}
