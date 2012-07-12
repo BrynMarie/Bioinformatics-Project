9dea7ad9c76f15b8fe242f8923a8a30c3b21ff85
BrynMarie <bronwynmr@hotmail.com>
2012-07-12 17:00:05 -0300

    Update master

--- ResidueToSS.java
+++ ResidueToSS.java
@@ -10,6 +10,28 @@ public class ResidueToSS {
     //TODO: Filter residues with too high of a zscore
     //look over coding ideas docs before going too far on this one.
     public ResidueToSS(ArrayList<Residue> resArray) {
+        oldRes = resArray.get(0);
+        ArrayList<Residue> currentInSS = new ArrayList<Residue>();
+        ArrayList<SecondaryStructure> ssArray = new ArrayList<SecondaryStructure>();
+        currentInSS.add(oldRes);
+        for (int i=1; i<resArray.size(); ++i) {
+            currentRes = resArray.get(i);
+            
+            //the old and current res are in the same ss, and current res isn't missing
+            if(currentRes.getSS().equals(oldRes.getSS() && !currentRes.isMissing()) {
+                currentInSS.add(currentRes);
+            }
+            //the old and current res are in different ss's, and the current res isn't missing
+            else if(!currentRes.getSS().equals(oldRes.getSS())) {
+                //add currentInSS to the ssArray
+                //if missing, marker for next not to exist
+            }
+            // they are in the same ss and the current res is missing
+            else if(currentRes.getSS().equals(oldRes.getSS()){
+                currentInSS.clear();
+                ssArray.add(new SecondaryStructure(currentRes.getSS(),false));
+            }
+        }
     }
 
 }
