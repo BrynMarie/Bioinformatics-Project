91cfbfe81e1b711d7db73039182ce21cd22fda48
BrynMarie <bronwynmr@hotmail.com>
2012-07-12 17:06:00 -0300

    Updated comments

--- ResidueToSS.java
+++ ResidueToSS.java
@@ -3,17 +3,21 @@ import java.io.*;
 
 public class ResidueToSS {
 
-    //this class will take an array of residues, some of which are missing in the dssp file or the pdb file. It has not been filtered of loops that are too long, either.
+    //this class will take an array of residues, some of which are missing in the dssp file or the pdb file. 
+    //It has not been filtered of loops that are too long, either.
 
-    //TODO: Filter out loops that are greater than 12 residues long
-    //TODO: Discard loops with missing residues and/or main chain atoms
-    //TODO: Filter residues with too high of a zscore
+    //TODO: Filter out loops that are greater than 12 residues long w/ loopCounter
+    //TODO: Discard loops with missing residues and/or main chain atoms w/ coding if/else's
+    //TODO: Filter residues with too high of a zscore // mark those as missing in the residue file bitte
     //look over coding ideas docs before going too far on this one.
     public ResidueToSS(ArrayList<Residue> resArray) {
         oldRes = resArray.get(0);
         ArrayList<Residue> currentInSS = new ArrayList<Residue>();
         ArrayList<SecondaryStructure> ssArray = new ArrayList<SecondaryStructure>();
         currentInSS.add(oldRes);
+        
+        loopCounter = 0;
+        
         for (int i=1; i<resArray.size(); ++i) {
             currentRes = resArray.get(i);
             
