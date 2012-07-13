9d1858c3b9aa2d3083970abf7d7565145106e0b8
Bryn <Bryn@Server.com>
2012-07-13 10:20:48 -0400

    adding sstosmotif.java

---
+++ SSToSmotif.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class SSToSmotif {
+    
+    // takes an arraylist of secondary structures
+    // uses length of secondary structures, connected-ness, and coords (pmoi)
+    //in order to glean information about geometric structures.
+
+    public SSToSmotif(ArrayList<SecondaryStructure> ssList) {
+	ArrayList<SecondaryStructure> ssArray = ssList;
+
+    }
+}
