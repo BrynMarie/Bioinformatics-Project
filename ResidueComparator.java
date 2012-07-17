4e34240754c2e7e9ea767f120e6889dcfb6d3dae
BrynMarie <bronwynmr@hotmail.com>
2012-07-17 17:50:40 -0300

    Making sure types compatible

--- ResidueComparator.java
+++ ResidueComparator.java
@@ -4,7 +4,9 @@ import java.io.*;
 public class ResidueComparator implements Comparator<Residue> {
 
     public int compare(Residue r1, Residue r2) {
-	return r1.getResNum().compareTo(r2.getResNum());
+    	Integer r1i = Integer.parseInt(r1.getResNum());
+    	Integer r2i = Integer.parseInt(r2.getResNum());
+	return r1i.compareTo(r2i);
     }
 
     public boolean equals(Object obj) {
