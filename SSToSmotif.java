421e2d31d7fbf7218c1df8355073676d78b167b0
BrynMarie <bronwynmr@hotmail.com>
2012-07-17 18:35:05 -0300

    Write to file code added.

--- SSToSmotif.java
+++ SSToSmotif.java
@@ -4,11 +4,27 @@ import java.util.*;
 public class SSToSmotif {
     
     // takes an arraylist of secondary structures
-    // uses length of secondary structures, connected-ness, and coords (pmoi)
-    //in order to glean information about geometric structures.
+    // prints out final information
 
-    public SSToSmotif(ArrayList<SecondaryStructure> ssList) {
-	ArrayList<SecondaryStructure> ssArray = ssList;
 
-    }
+	public SSToSmotif(ArrayList<SecondaryStructure> ssList, CalcGeo geometries) {
+		ArrayList<SecondaryStructure> ssArray = ssList;
+		
+		printOutInformation(ssArray, geometries);
+	}
+	
+	// will be replaced by a print to file eventually
+	public void printOutInformation(ArrayList<SecondaryStructure> ssArray, CalcGeo geometries) {
+		// we need to sync up the information from the ssarray and the geometries somehow or this is going to be a mess.
+		
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter(args[0]));
+			
+			for(int i = 0; i < ssArray.size(); ++i) {
+				out.println("Put information here");
+			}
+			out.close();
+		}
+		catch (IOException ioe) { e.printStackTrace(); }		
+	}
 }
