7d867176a828fa557a0f08d85a44024558d71e88
BrynMarie <bronwynmr@hotmail.com>
2012-07-18 16:25:06 -0300

    Completed w/ Comments

--- AnalyzeOneProtein.java
+++ AnalyzeOneProtein.java
@@ -1,11 +1,24 @@
+/**
+ * Programmers: Bryn Reinstadler and Jennifer Van
+ * Date: July 18th, 2012
+ * Filename: AnalyzeOneProtein.java
+ * 
+ * Purpose: Read one pdb file and one dssp file to string arrays,
+ * after which the string arrays are sent into the sieve-like
+ * infrastructure starting with ParseFiles.
+ * */
+
 import java.util.*;
 import java.io.*;
 
 public class AnalyzeOneProtein {
 
-    ArrayList<String> dsspFile;
-    ArrayList<String> pdbFile;
+    public static ArrayList<String> dsspFile;
+    public static ArrayList<String> pdbFile;
     
+    /* The constructor method of AnalyzeOneProtein reads in a dssp file and a pdb file 
+    	and then sends them both to be parsed. Right now the files are hard-coded in.
+    	*/
     public AnalyzeOneProtein(){
 	//read in single file
 	dsspFile = readFile("C:/Users/Bryn/Documents/CodingProjects/dssp/1a00.dssp");
@@ -15,31 +28,34 @@ public class AnalyzeOneProtein {
 	ParseFiles f2 = new ParseFiles(dsspFile, pdbFile);
     }
 
+	/* The readFile method takes a the string of a file, opens it, and scans each line in the file
+		to a new string in a string arraylist. */
     public ArrayList<String> readFile(String arg) {
-
-	String thisLine;
-	ArrayList<String> fileToRead = new ArrayList<String>();
-	try {
-	    //BufferedReader to scan in the file
-	    BufferedReader buffy = new BufferedReader(new FileReader(arg));
-	    while((thisLine = buffy.readLine()) != null) { //while there are still lines in the document
-		fileToRead.add(thisLine);
-	    } //end while
-	} // end try
-	catch (IOException ioe) {
-	    System.err.println("Error: " + ioe);
-	    fileToRead.clear();
-	}
-	return fileToRead;
+		String thisLine;
+		ArrayList<String> fileToRead = new ArrayList<String>();
+		try {
+		    //BufferedReader to scan in the file
+		    BufferedReader buffy = new BufferedReader(new FileReader(arg));
+		    while((thisLine = buffy.readLine()) != null) { //while there are still lines in the document
+				fileToRead.add(thisLine);
+	    	} //end while
+		} // end try
+		catch (IOException ioe) {
+		    System.err.println("Error: " + ioe);
+		    fileToRead.clear();
+		}
+		return fileToRead;
     }
 
+    /* The printOut method takes an ArrayList of Strings and prints out each element of it. */
     public void printOut(ArrayList<String> al) {
-	for (int i=0; i<al.size(); ++i){
-	    System.out.println(al.get(i));
-	}
+		for (int i=0; i<al.size(); ++i){
+		    System.out.println(al.get(i));
+		}
     }
-
+	
+	/* Main method */
     public static void main(String[] args){
-	AnalyzeOneProtein ap1 = new AnalyzeOneProtein();
+		AnalyzeOneProtein ap1 = new AnalyzeOneProtein();
     }
 }
