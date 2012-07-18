153a7e99fd64423a861cb878628182ad5d4c5e3a
BrynMarie <bronwynmr@hotmail.com>
2012-07-18 16:19:10 -0300

    (later) Change constructors throughout, check use of methods.

--- SecondaryStructure.java
+++ SecondaryStructure.java
@@ -1,61 +1,74 @@
+/**
+ * Programmers: Bryn Reinstadler and Jennifer Van
+ * Date: July 18th 2012
+ * Filename: SecondaryStructure.java
+ * 
+ * Purpose: This file is an object that is meant to carry information about secondary structures, including
+ * the type of secondary structure, residues in the secondary structure, the length, and whether or not,
+ * by the criteria of Fuentes-Fernandez, the secondary structure should be counted as existing.
+ * */
+
 import java.io.*;
 import java.util.*;
 
 public class SecondaryStructure {
 
     String sstype;
-    int length;
     ArrayList<Residue> resArray = new ArrayList<Residue>();
+    int length;
     boolean exists;
 
-    public SecondaryStructure(String ss, int length, ArrayList<Residue> resArray) {
-    	sstype = ss;
+	/* A constructor for Secondary Structure that provides information on 
+		type, length, and the residues within. */
+    public SecondaryStructure(String sstype, int length, ArrayList<Residue> resArray) {
+    	this.sstype = sstype;
     	this.length = length;
     	this.resArray = resArray;
     }
     
-    public SecondaryStructure(String ss, boolean exists) {
-    	sstype = ss;
+    public SecondaryStructure(String sstype, boolean exists) {
+    	this.sstype = sstype;
     	this.exists = exists;
     }
 
     //Accessor fields
     public String getSSType() {
-	return sstype;
+		return sstype;
     }
-
-    public boolean exists() {
-	return exists;
-    }
-
+    
     public int length() {
-	return length;
+		return length;
     }
     
     public Residue firstResidue() {
     	return resArray.get(0);
     }
-    
+
+    public boolean exists() {
+		return exists;
+    }
+
+	// is this ever used?
     public Residue lastResidue() {
     	return resArray.get(resArray.size() - 1);
     }
 
     //Mutator fields
     public void setSSType(String arg) {
-	if(arg.equals("H") || arg.equals("S") || arg.equals("T")) {
-	    sstype = arg;
-	}
-	else {
-	    sstype = ""; //throw an exception
-	}
+		if(arg.equals("H") || arg.equals("S") || arg.equals("T")) {
+		    sstype = arg;
+		}
+		else {
+		    sstype = ""; //throw an exception
+		}
     }
 
     public void setExists(boolean state) {
-	exists = state;
+		exists = state;
     }
 
     public void setLength(int newLength) {
-	length = newLength;
+		length = newLength;
     }
     
     public void addResidue(Residue res){
