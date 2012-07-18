30cbb7ab8758021f1566199fbf0d223beede91a6
sugargreenbean <minh.j.van@gmail.com>
2012-07-18 01:52:47 -0300

    compiles--need to check correctness

--- AtomToResidue.java
+++ AtomToResidue.java
@@ -4,23 +4,26 @@ import java.lang.*;
 
 public class AtomToResidue {
 	
+	public static ArrayList<Atom> atomList;
+	public static ArrayList<Residue> pmoiArray;
+
 	//takes as input an unsorted arraylist of atoms
 	public AtomToResidue(ArrayList<Atom> al, ArrayList<String> dsspFile, double bFactorMean, double bFactorSTD) {
-		this.atomList = al;
+		atomList = al;
 		Collections.sort(atomList, new AtomComparator());
 		ArrayList<Residue> resArray = turnIntoResidueArray(atomList, dsspFile, bFactorMean, bFactorSTD);
-		pmoiArray = f5.newResArray;
+		pmoiArray = CalculatePMOI.newResArray;
 		ResidueToSS f4 = new ResidueToSS(resArray, pmoiArray);
 	}
     
 	public ArrayList<Residue> turnIntoResidueArray(ArrayList<Atom> atomList, 
 		ArrayList<String> dsspFile, double bFactorMean, double bFactorSTD) {
 		
-		int residueAtoms, newResNum;
-		double currentResidueBFactor, meanOfCurrentResidue, zScore;
+		int residueAtoms=0, newResNum;
+		double currentResidueBFactor=0, meanOfCurrentResidue, zScore;
 		long countResidues = 0;
 		boolean cTerm = false, nTerm = false;
-		int currentResNum = atomList.get(0).getResNum();
+		int currentResNum = Integer.parseInt(atomList.get(0).getResNum());
 		Atom curAtom = atomList.get(0); 
 		Atom newAtom;
 		ArrayList<Residue> resArray = new ArrayList<Residue>();
@@ -31,7 +34,7 @@ public class AtomToResidue {
 		
 		for (int i = 0; i<atomList.size(); ++i) {
 			newAtom = atomList.get(i);
-			newResNum = newAtom.getResNum();
+			newResNum = Integer.parseInt(newAtom.getResNum());
 			
 			//if we're still on the same residue as before...
 			if (newResNum == currentResNum) {
@@ -49,16 +52,6 @@ public class AtomToResidue {
 
 				//String pdbResNum, double bFactor, String ssType, 
     				//boolean nTerm, boolean cTerm, ArrayList<Atom> atomList, CartesianCoord pmoi
-				
-				//if zscore is too high set as missing HERE
-				/* if(zScore < ____________ ) { // zscore falls into parameters
-					resArray.add(new Residue(currentResNum, zScore, "",
-					nTerm, cTerm, currentlyInResidue, 0));
-					//set pmoi to 0
-				}
-				else {
-					resArray.add(new Residue(currentResNum, false));
-				}*/
 
 				currentlyInResidue.clear();
 				currentResNum = newResNum;
@@ -68,21 +61,25 @@ public class AtomToResidue {
 				nTerm = atomList.get(i).getNTerm();
 			}
 		}
-
+		
 		Collections.sort(tempArray, new ResidueComparator());
 		Collections.sort(resArray, new ResidueComparator());
 		ArrayList<Residue> finalResArray;
 		
 		if (Integer.parseInt(tempArray.get(0).getResNum()) < Integer.parseInt(resArray.get(0).getResNum())) {
-			finalResArray = mergeArrays(tempArray, resArray, true);	
+			finalResArray = mergeArrays(tempArray, resArray, pmoiArray, true);	
 		}
 		else {
-			finalResArray = mergeArrays(resArray, tempArray, false);
+			finalResArray = mergeArrays(resArray, tempArray, pmoiArray, false);
 		}
 		
 		return finalResArray;
 	}
     
+	public double zScore(double currentMean, double totalMean, double totalStdDev) {
+		double zScore = (currentMean - totalMean) / totalStdDev;
+		return zScore;
+	}
       
 	public ArrayList<Residue> mergeArrays(ArrayList<Residue> lowerArray, ArrayList<Residue> higherArray, 
 		ArrayList<Residue> pmoiArray, boolean ssFirst) {
@@ -125,7 +122,7 @@ public class AtomToResidue {
 				hResNum = Integer.parseInt(currentHigher.getResNum());
 			} // end other while
 			
-			while (lResNum == hResNum && lResNum == pResNum) {
+			while (lResNum == hResNum) {
 				finalResArray.add(mergeResidues(currentLower, currentHigher, ssFirst));	
 			} // end while
 		} // end for
@@ -133,7 +130,7 @@ public class AtomToResidue {
 		return finalResArray;
 	} // end method
     
-	public Residue mergeResidues(Residue res1, Residue res2, Residue res3, boolean ssFirst) {
+	public Residue mergeResidues(Residue res1, Residue res2, boolean ssFirst) {
 		if(!ssFirst) {
 			Residue temp = res2;
 			res2 = res1;
@@ -146,7 +143,7 @@ public class AtomToResidue {
 		boolean cTerm = res2.getCTerm();
 		boolean nTerm = res2.getNTerm();
 		ArrayList<Atom> aL = res2.getAtomList();
-		CartesianCoord holder;
+		CartesianCoord holder = new CartesianCoord (0, 0, 0);
 		
 		//String pdbResNum, double bFactor, String ssType, 
     		//boolean nTerm, boolean cTerm, ArrayList<Atom> atomList, CartesianCoord pmoi
