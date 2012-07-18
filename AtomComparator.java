0b30b88f5540cfd5ae0c9aa36f726257b9dcefb4
BrynMarie <bronwynmr@hotmail.com>
2012-07-18 16:10:12 -0300

    Complete.

--- AtomComparator.java
+++ AtomComparator.java
@@ -1,15 +1,21 @@
 import java.util.*;
 import java.io.*;
 
-//to be used to sort atoms according to which residue they are in.
+/**
+ * Sorts atoms by pdb residue number
+ * */
 public class AtomComparator implements Comparator<Atom> {
     
+    /* Compares two atoms and returns the one with -, 0, or + 
+    	depending on whether a1 is less than, equal to, or greater than a2.
+    */
     public int compare(Atom a1, Atom a2) {
         Integer parsed1 = Integer.parseInt(a1.getResNum());
 	Integer parsed2 = Integer.parseInt(a2.getResNum());
 	return parsed1.compareTo(parsed2);
     }
     
+    /* Returns true only if both objects point are identical */
     public boolean equals(Object obj){
 	return this.equals(obj);
     }
