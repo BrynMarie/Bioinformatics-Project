d05de492a038ca1c45f11609a235ce965dd916d3
sugargreenbean <minh.j.van@gmail.com>
2012-07-27 08:35:43 -0300

    recalculated pmoi
    according to wikipedia. it returns feasible numbers now

--- CalculatePMOI.java
+++ CalculatePMOI.java
@@ -1,177 +1,148 @@
-/** 
- * Programmer: Jennifer Van and Bryn Reinstadler
- * Date: July 25th 2012
- * Filename: CalculatePMOI.java
- * 
- * Purpose: Takes the main residue array of all residues in the protein and uses
- * nterm and cterm information to calculate the principal moment of inertia
- * of each c and nterm in the protein.
- * */
-
-
 import java.io.*;
 import java.util.*;
+
 import Jama.*;
 
 public class CalculatePMOI {
 
-    static ArrayList<Residue> newResArray;
-    
-    // NEW PMoI variable declarations
-    int pastResidue = 0, currentResidue=0;
-    double firstTermOfIxx = 0, firstTermOfIyy = 0, firstTermOfIzz = 0, firstTermOfIxy = 0, firstTermOfIxz = 0, firstTermOfIzx = 0, firstTermOfIyx = 0, firstTermOfIyz = 0, firstTermOfIzy = 0;   
-    double secondTermOfIxx = 0, secondTermOfIyy = 0, secondTermOfIzz = 0, secondTermOfIxy = 0, secondTermOfIxz = 0, secondTermOfIzx = 0, secondTermOfIyx = 0, secondTermOfIyz = 0, secondTermOfIzy = 0;
-    double thirdTermOfIxx = 0, thirdTermOfIyy = 0, thirdTermOfIzz = 0, thirdTermOfIxy = 0, thirdTermOfIxz = 0, thirdTermOfIzx = 0, thirdTermOfIyx = 0, thirdTermOfIyz = 0, thirdTermOfIzy = 0;
-    
-    double Ixx = 0, Iyy = 0, Izz = 0, Ixy = 0, Iyx = 0, Ixz = 0, Izx = 0, Iyz = 0, Izy = 0;
-    double totalAtomicWeight = 0;
-    int countBFactorGreaterThanOne = 0;
-    boolean discardBFactor = false;
+	static ArrayList<Residue> newResArray;
+	int countBFactorGreaterThanOne = 0;
+	boolean discardBFactor = false;
 
-    public CalculatePMOI(ArrayList<Residue> residueList) {
-	this.newResArray = calcPMOI(residueList);
-    }
+	public CalculatePMOI(ArrayList<Residue> residueList) {
+		this.newResArray = calcPMOI(residueList);
+	}
 
-    public ArrayList<Residue> calcPMOI(ArrayList<Residue> residueList) {
-	newResArray = new ArrayList<Residue>();
-	int newC = 0;
-	boolean nt, ct;
-	for (int i = 0; i<residueList.size(); ++i) {
-	    ct = residueList.get(i).getCTerm();
-	    nt = residueList.get(i).getNTerm();
-	    if(ct) { ++newC; }
-	    if(nt) { ++newC; }
-	    while(newC > 0) {
-		--newC;
-		// if C-terminus or N-terminus => calculate PMOI
-		//set up calculations for Ixx term by term
-		//Has been abstracted to a pretty clear point
-		//this also ideally will be in a helper method. 
-		ArrayList<Atom> currentAtomListOfResidue = residueList.get(i).getAtomList();
-		String pdbNum = residueList.get(i).getResNum();
-		String sstype = residueList.get(i).getSS();
-		
-		for(int j = 0; j<currentAtomListOfResidue.size(); j++){
-			Atom currentAtom = currentAtomListOfResidue.get(j);
-			double aw = getAtomicWeight(currentAtom);
-			double x = currentAtom.getCoords().getX();
-			double y = currentAtom.getCoords().getY();
-			double z = currentAtom.getCoords().getZ();
-			double xSq = Math.pow(x,2);
-			double ySq = Math.pow(y,2);
-			double zSq = Math.pow(z,2);
-			// calc Ixx term by term
-			firstTermOfIxx += (aw) * (ySq + zSq);
-			secondTermOfIxx += Math.pow((aw * y), 2);
-			thirdTermOfIxx += Math.pow((aw * z), 2);
-			//calc Iyy term by term
-			firstTermOfIyy += aw * (xSq + zSq);
-			secondTermOfIyy += Math.pow((aw * x),2);
-			thirdTermOfIyy += Math.pow((aw * z), 2);
-			//set up calculations for Izz term by term
-			firstTermOfIzz += aw * (xSq + ySq);
-			secondTermOfIyy += Math.pow((aw * x), 2);
-			thirdTermOfIyy += Math.pow((aw * y), 2);
-			//set up calculations for Ixy/Iyx term by term
-			firstTermOfIxy += aw * x * y;
-			secondTermOfIxy += aw * x;
-			thirdTermOfIxy += aw * y;
-			//set up calculations for Ixz/Izx term by term
-			firstTermOfIxz += aw * x * z;
-			secondTermOfIxz += aw * x;
-			thirdTermOfIxz += aw * z;
-			//set up calculations for Iyz/Izy term by term
-			firstTermOfIxz += aw * y * z;
-			secondTermOfIxz += aw * y;
-			thirdTermOfIxz += aw * z;
-			// calculate total sum of atom weights of a C-terminus/N-terminus for later calculation
-			totalAtomicWeight += aw; } //end iterating through the list of atoms of a residue
-			//BEGIN calculate PMoI
-			Ixx = firstTermOfIxx - (1 / totalAtomicWeight) * (secondTermOfIxx) - (1 / totalAtomicWeight)
-		    * (thirdTermOfIxx);
-			Iyy = firstTermOfIyy - (1 / totalAtomicWeight)	* (secondTermOfIyy) - (1 / totalAtomicWeight)
-		    * (thirdTermOfIyy);
-			Izz = firstTermOfIzz - (1 / totalAtomicWeight)
-		    * (secondTermOfIzz) - (1 / totalAtomicWeight)
-		    * (thirdTermOfIzz);
-			Ixy = -firstTermOfIxy + (1 / totalAtomicWeight)
-		    * (secondTermOfIxy) * (thirdTermOfIxy);
-			Iyx = Ixy;
-			Ixz = -firstTermOfIxz + (1 / totalAtomicWeight)	* (secondTermOfIxz) * (thirdTermOfIxz);
-			Izx = Ixz;
-			Iyz = -firstTermOfIyz + (1 / totalAtomicWeight) * (secondTermOfIyz) * (thirdTermOfIyz);
-			Izy = Iyz;
-		
-		double[][] populateMatrix = new double[][] { { Ixx, Ixy, Izz },{ Iyx, Iyy, Iyz }, { Izx, Izy, Izz } };
-		Matrix matrixForEigen = new Matrix(populateMatrix);
-		EigenvalueDecomposition ed = matrixForEigen.eig();
-		double[] getRealEigenvalues = ed.getRealEigenvalues();
-		double[] getImgEigenvalues = ed.getImagEigenvalues(); 
-		CartesianCoord principalMomentsOfInertia = new CartesianCoord(getRealEigenvalues[0], getRealEigenvalues[1], getRealEigenvalues[2]);
-		//reset terms
-		Ixx=0;
-		Iyy=0; 
-		Izz=0; 
-		Ixy=0; 
-		Iyx=0; 
-		Ixz=0;
-		Izx=0; 
-		Izy=0; 
-		Iyz=0;
-		//reset first terms
-		firstTermOfIxx=0; 
-		firstTermOfIyy=0; 
-		firstTermOfIzz=0;
-		firstTermOfIxy=0; 
-		firstTermOfIyx=0; 
-		firstTermOfIxz=0; 
-		firstTermOfIzx=0; 
-		firstTermOfIzy=0; 
-		firstTermOfIyz=0;
-		//reset second terms
-		secondTermOfIxx=0; 
-		secondTermOfIyy=0; 
-		secondTermOfIzz=0; 
-		secondTermOfIxy=0; 
-		secondTermOfIyx=0; 
-		secondTermOfIxz=0; 
-		secondTermOfIzx=0; 
-		secondTermOfIzy=0; 
-		secondTermOfIyz=0;
-		//reset third terms
-		thirdTermOfIxx=0; 
-		thirdTermOfIyy=0; 
-		thirdTermOfIzz=0; 
-		thirdTermOfIxy=0; 
-		thirdTermOfIyx=0; 
-		thirdTermOfIxz=0; 
-		thirdTermOfIzx=0; 
-		thirdTermOfIzy=0;
-		thirdTermOfIyz=0;
-		totalAtomicWeight=0;
-		//end calculate PMoI
-		//need xyz coordinates to calculate geometries
+	//calculate principal moment of inertia
+	public ArrayList<Residue> calcPMOI(ArrayList<Residue> residueList) {
+		newResArray = new ArrayList<Residue>();
+		int bc = 0;
+		int newC = 0;
+		boolean nt, ct;
+		for (int i = 0; i < residueList.size(); ++i) {
+			ArrayList<Atom> atomList = residueList.get(i).getAtomList();
+			ct = residueList.get(i).getCTerm();
+			nt = residueList.get(i).getNTerm();
+			if (ct) {
+				++newC;
+			}
+			if (nt) {
+				++newC;
+			}
+			while (newC > 0) {
+				--newC;
+				++bc;
+				// if C-terminus or N-terminus => calculate PMOI
+				//get relevant info for residue construction
+				ArrayList<Atom> currentAtomListOfResidue = residueList.get(i).getAtomList();
+				String pdbNum = residueList.get(i).getResNum();
+				String sstype = residueList.get(i).getSS();
+				
+				//calculate total mass of residue
+				double totalMassOfResidue = calcMassResidue(atomList);
+				
+				//calc geo center of protein
+				CartesianCoord centroid = calcCentroid(atomList, totalMassOfResidue);
+				
+				//calc inertia tensor matrix
+				double[][] it = calcInertiaTensor(atomList, centroid);
+				
+				//get principal moments of inertia from it matrix
+				double xCoordPMOI = it[0][0];
+				double yCoordPMOI = it[1][1];
+				double zCoordPMOI = it[2][2];
+				
+				//create principal moment of inertia cartesian coord object
+				CartesianCoord principalMomentsOfInertia = new CartesianCoord(xCoordPMOI, yCoordPMOI, zCoordPMOI);
 
-		newResArray.add(new Residue(pdbNum, principalMomentsOfInertia, 
-					    currentAtomListOfResidue, sstype, nt, ct));
-	    }// end if
-	}// end for
-	return newResArray;
-    }// end method
+				//create residue list
+				newResArray.add(new Residue(pdbNum, principalMomentsOfInertia,
+						currentAtomListOfResidue, sstype, nt, ct));
+			}
+		}
+		return newResArray;
+	}// end method
 
+	public static double getAtomicWeight(Atom currentAtom) { // NEW PMoI method to get
+														// atomic weight given
+														// atom type
 
-    public double getAtomicWeight(Atom currentAtom) { //NEW PMoI method to get atomic weight given atom type
+		double atomWeight = 0;
+		String atomType = Character.toString(currentAtom.getAtomType()
+				.charAt(0));
+		String[] atomTypeArray = { "H", "N", "C", "O", "S" };
+		double[] atomWeightArray = { 1.00794, 14.0067, 12.0107, 15.9994, 32.065 };
 
-	double atomWeight = 0;
-	String atomType = Character.toString(currentAtom.getAtomType().charAt(0));
-	String[] atomTypeArray = {"H","N","C","O","S"};
-	double[] atomWeightArray = {1.00794, 14.0067, 12.0107, 15.9994, 32.065};
+		for (int i = 0; i < atomTypeArray.length; ++i) {
+			if (atomType.equals(atomTypeArray[i])) {
+				atomWeight = atomWeightArray[i];
+			}
+		}
+		return atomWeight;
+	}
+
+	// calc centroid for calc pmoi
+	public static CartesianCoord calcCentroid(ArrayList<Atom> atomList, double totalMass) {
+		int n = atomList.size();
+		double xCoord = 0;
+		double yCoord = 0;
+		double zCoord = 0;
+		for (int i = 0; i < n; i++) {
+			Atom currentAtom = atomList.get(i);
+				xCoord += currentAtom.getX();
+				yCoord += currentAtom.getY();
+				zCoord += currentAtom.getZ();
+		}
+		xCoord = xCoord/n;
+		yCoord = yCoord/n;
+		zCoord = zCoord/n;
+		CartesianCoord centroidCoord = new CartesianCoord(xCoord, yCoord, zCoord);
+		return centroidCoord;
+	}
 	
-	for(int i=0; i<atomTypeArray.length; ++i) {
-	    if (atomType.equals(atomTypeArray[i])) {
-		atomWeight = atomWeightArray[i];
+	public static double calcMassResidue(ArrayList<Atom> atomList) {
+		double aw=0;
+		for (int i = 0; i < atomList.size(); i++) {
+			Atom currentAtom = atomList.get(i);
+			aw += getAtomicWeight(currentAtom);
+		}
+		return aw;
+	}
+	
+	//calc inertia tensor
+	 public static double[][] calcInertiaTensor(ArrayList<Atom> atomList, CartesianCoord com) {
+	        //initialize current atom coordinates
+		 	double x = 0;
+	        double y = 0;
+	        double z = 0;
+	        //initialize centroid coordinates
+	        double xOfCentroid = com.getX();
+	        double yOfCentroid = com.getY();
+	        double zOfCentroid = com.getZ();
+	        //initialize inertia tensor matrix
+	        double[][] it = new double[3][3];
+	        
+	        for(int i = 0; i<atomList.size(); i++){
+	            //subtract the centroid from coordinates
+	        	Atom currentAtom = atomList.get(i);
+	            x = currentAtom.getX() - xOfCentroid;
+	            y = currentAtom.getY() - yOfCentroid;
+	            z = currentAtom.getZ() - zOfCentroid;
+	            
+	            //populate matrix
+	            it[0][0] = it[0][0] + (y*y + z*z);
+	            it[1][1] = it[1][1] + (x*x + z*z);
+	            it[2][2] = it[2][2] + (x*x + y*y);
+	            
+	            it[0][1] = it[0][1] - (x * y);
+	            it[0][2] = it[0][2] - (x * z);
+	            it[1][2] = it[1][2] - (y * z);
+	        }
+	        it[1][0] = it[0][1];
+	        it[2][1] = it[1][2];
+	        it[2][0] = it[0][2];
+	        return it;
 	    }
-	}	
-	return atomWeight;
-    }
+
+
 }// end class
